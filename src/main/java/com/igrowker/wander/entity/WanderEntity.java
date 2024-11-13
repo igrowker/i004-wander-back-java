d1f71f4665a8b4d20402954aecc0f64a8aaded51
Elías Barrera <eliasbarreramail@gmail.com>
2024-11-13 16:25:25 +0100

    Feat: add lombok annotations on entity.
    - delete data.sql archive

--- src/main/java/com/igrowker/wander/entity/WanderEntity.java
+++ src/main/java/com/igrowker/wander/entity/WanderEntity.java
@@ -2,18 +2,15 @@ package com.igrowker.wander.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 //Provisional class to generate the MVC structure
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WanderEntity {
 	 @Id
 	    private Long id;
-	 
-	 // Getters & Setters
-	    public Long getId() {
-	        return id;
-	    }
-
-	    public void setId(Long id) {
-	        this.id = id;
-	    }
 }
