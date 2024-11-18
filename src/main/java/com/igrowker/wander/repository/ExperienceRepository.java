81f395d426231a7f40efb0a1c0625e5be3c17cdf
Elías Barrera <ebdev81@gmail.com>
2024-11-18 10:48:29 +0100

    feat: add GET endpoint for retrieving experiences with filtering options
    - Implemented new GET endpoint `/experiences` in `ExperienceController`
    to retrieve experiences with optional filters: location, type, and
    maxPrice.
    - Added method `getExperiences` in `ExperienceService` and its
    implementation in `ExperienceServiceImpl` to handle filtered queries.
    - Defined additional query methods in `ExperienceRepository` to support
    filtering based on provided criteria.

--- src/main/java/com/igrowker/wander/repository/ExperienceRepository.java
+++ src/main/java/com/igrowker/wander/repository/ExperienceRepository.java
@@ -1,5 +1,7 @@
 package com.igrowker.wander.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -7,5 +9,21 @@ import com.igrowker.wander.entity.ExperienceEntity;
 
 @EnableMongoRepositories(basePackages = "com.igrowker.wander.repository")
 public interface ExperienceRepository extends MongoRepository<ExperienceEntity, String> {
+	
+    // Method to find experiences by location
+    List<ExperienceEntity> findByLocation(String location);
+
+    // Method to find experiences by type
+    List<ExperienceEntity> findByType(String type);
+
+    // Method to find experiences with a maximum price
+    List<ExperienceEntity> findByPriceLessThanEqual(Double maxPrice);
+
+    // Method to find experiences by location and type
+    List<ExperienceEntity> findByLocationAndType(String location, String type);
+
+    // Method to find experiences by location, type, and maximum price
+    List<ExperienceEntity> findByLocationAndTypeAndPriceLessThanEqual(String location, String type, Double maxPrice);
 
 }
+
