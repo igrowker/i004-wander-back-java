48093c573889b1cbed3d3485f0cd8e874c09d579
Manuela Fernandez <manu.fer.0396@gmail.com>
2024-11-18 21:40:00 -0300

    user profile update with validations

--- src/main/java/com/igrowker/wander/controller/UserController.java
+++ src/main/java/com/igrowker/wander/controller/UserController.java
@@ -2,11 +2,14 @@ package com.igrowker.wander.controller;
 
 import com.igrowker.wander.dto.RegisterUserDto;
 import com.igrowker.wander.dto.ResponseUserDto;
+import com.igrowker.wander.entity.User;
 import com.igrowker.wander.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 
 @RestController
@@ -23,5 +26,24 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
 
     }
+
+    /**
+     * Update the authenticated user's profile
+     * @param userUpdates user data to update
+     * @return updated user profile
+     */
+    @PutMapping("/profile")
+    public ResponseEntity<User> updateUserProfile(
+            @RequestBody User userUpdates) {
+        try {
+            User updatedUser = userService.updateUserProfile(userUpdates);
+            return ResponseEntity.ok(updatedUser);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
 
 }
