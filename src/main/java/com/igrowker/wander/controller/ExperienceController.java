65bf1adbbbba730787d29b1ddc2f92680dc5a2b9
Elías Barrera <ebdev81@gmail.com>
2024-11-20 21:22:20 +0100

    Feat: Create RequestExperience with validations and modify service

--- src/main/java/com/igrowker/wander/controller/ExperienceController.java
+++ src/main/java/com/igrowker/wander/controller/ExperienceController.java
@@ -1,11 +1,14 @@
 package com.igrowker.wander.controller;
 
+import com.igrowker.wander.dto.experience.RequestExperienceDto;
 import com.igrowker.wander.entity.ExperienceEntity;
+import com.igrowker.wander.entity.User;
 import com.igrowker.wander.service.ExperienceService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,73 +19,64 @@ import javax.validation.Valid;
 @RequestMapping("/experiences")
 public class ExperienceController {
 
-    // Service injection to manage business logic
-    @Autowired
-    private ExperienceService experienceService;
+	// Service injection to manage business logic
+	@Autowired
+	private ExperienceService experienceService;
 
-    /**
-     * Endpoint to create a new experience
-     * @param experience Object received from the BFF with the experience data
-     * @return Response indicating if the experience was created successfully
-     */
-    @PostMapping
-    public ResponseEntity<String> createExperience(@Valid @RequestBody ExperienceEntity experience) {
-        try {
-            // Delegate the creation of experience to the service layer
-            experienceService.createExperience(experience);
-            return ResponseEntity.status(HttpStatus.CREATED).body("Experiencia creada con éxito");
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error en los datos de la experiencia: " + e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error al crear la experiencia: " + e.getMessage());
-        }
-    }
+	/**
+	 * Endpoint to create a new experience
+	 * 
+	 * @param experience Object received from the BFF with the experience data
+	 * @return Response indicating if the experience was created successfully
+	 */
+	@PostMapping
+	public ResponseEntity<ExperienceEntity> createExperience(@Valid @RequestBody RequestExperienceDto experience,
+			Authentication authentication) {
+		User user = (User) authentication.getPrincipal();
 
-    // Endpoint to list experiences
-    @GetMapping
-    public ResponseEntity<List<ExperienceEntity>> getExperiences(
-            @RequestParam(required = false) String location,
-            @RequestParam(required = false) Double maxPrice
-    ) {
-        try {
-            List<ExperienceEntity> experiences = experienceService.getExperiences(location, maxPrice);
-            return ResponseEntity.ok(experiences);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(null);
-        }
-    }
-    
-    // Endpoint to get experience by ID
-    @GetMapping("/{id}")
-    public ResponseEntity<ExperienceEntity> getExperienceById(@PathVariable String id) {
-        try {
-            ExperienceEntity experience = experienceService.getExperienceById(id);
-            return ResponseEntity.ok(experience);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
-    }
+		ExperienceEntity savedexperience = experienceService.createExperience(experience, user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedexperience);
 
- // Endpoint to update an experience by ID
-    @PutMapping("/{id}")
-    public ResponseEntity<ExperienceEntity> updateExperience(
-            @PathVariable String id,
-            @Valid @RequestBody ExperienceEntity newExperienceData
-    ) {
-        try {
-            ExperienceEntity updatedExperience = experienceService.updateExperience(id, newExperienceData);
-            return ResponseEntity.ok(updatedExperience);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } catch (SecurityException e) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
-    }
+	}
+
+	// Endpoint to list experiences
+	@GetMapping
+	public ResponseEntity<List<ExperienceEntity>> getExperiences(@RequestParam(required = false) String location,
+			@RequestParam(required = false) Double maxPrice) {
+		try {
+			List<ExperienceEntity> experiences = experienceService.getExperiences(location, maxPrice);
+			return ResponseEntity.ok(experiences);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
+
+	// Endpoint to get experience by ID
+	@GetMapping("/{id}")
+	public ResponseEntity<ExperienceEntity> getExperienceById(@PathVariable String id) {
+		try {
+			ExperienceEntity experience = experienceService.getExperienceById(id);
+			return ResponseEntity.ok(experience);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
+
+	// Endpoint to update an experience by ID
+	@PutMapping("/{id}")
+	public ResponseEntity<ExperienceEntity> updateExperience(@PathVariable String id,
+			@Valid @RequestBody ExperienceEntity newExperienceData) {
+		try {
+			ExperienceEntity updatedExperience = experienceService.updateExperience(id, newExperienceData);
+			return ResponseEntity.ok(updatedExperience);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		} catch (SecurityException e) {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
 }
-
--- src/main/java/com/igrowker/wander/service/ExperienceService.java
+++ src/main/java/com/igrowker/wander/service/ExperienceService.java
@@ -2,19 +2,16 @@ package com.igrowker.wander.service;
 
 import java.util.List;
 
-
+import com.igrowker.wander.dto.experience.RequestExperienceDto;
 import com.igrowker.wander.entity.ExperienceEntity;
+import com.igrowker.wander.entity.User;
 
 public interface ExperienceService {
-	//Method to create experiences
-    ExperienceEntity createExperience(ExperienceEntity experience);
+	ExperienceEntity createExperience(RequestExperienceDto experience, User user);
     
-    //Method to list experiences with filters
     List<ExperienceEntity> getExperiences(String location, Double maxPrice);
 
-    // Method to get an experience by ID
     ExperienceEntity getExperienceById(String id);
 
-    // Method to update an existing experience
     ExperienceEntity updateExperience(String id, ExperienceEntity updatedExperience);
 }
