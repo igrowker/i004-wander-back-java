86acd4f77928fe36ae3269220689c24fd15f38bd
Manuela Fernandez <manu.fer.0396@gmail.com>
2024-11-26 14:41:05 -0300

    Make get Experience endpoints public

--- src/main/java/com/igrowker/wander/security/SecurityConfiguration.java
+++ src/main/java/com/igrowker/wander/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.igrowker.wander.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,7 @@ public class SecurityConfiguration {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/experiences/**").permitAll()
                         .requestMatchers("/api/autenticacion/**", "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
