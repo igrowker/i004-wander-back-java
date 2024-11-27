f42b3d044def60cc175edd62215966eeb9b8a5dd
Manuela Fernandez <manu.fer.0396@gmail.com>
2024-11-27 12:52:26 -0300

    Add funcionality get experiences by host

--- src/main/java/com/igrowker/wander/service/ExperienceService.java
+++ src/main/java/com/igrowker/wander/service/ExperienceService.java
@@ -3,6 +3,7 @@ package com.igrowker.wander.service;
 import java.util.List;
 
 import com.igrowker.wander.dto.experience.RequestExperienceDto;
+import com.igrowker.wander.dto.experience.ResponseExperienceDto;
 import com.igrowker.wander.entity.ExperienceEntity;
 import com.igrowker.wander.entity.User;
 
@@ -14,10 +15,11 @@ public interface ExperienceService {
     ExperienceEntity getExperienceById(String id);
 
     ExperienceEntity updateExperience(String id, ExperienceEntity updatedExperience);
+    
+    List<ExperienceEntity> getExperiencesByTag(String tag);
+    
+    List<ExperienceEntity> getExperiencesByMultipleTags(List<String> tags);
 
-    List<ExperienceEntity> getLatestExperiences(int limit);
+    List<ResponseExperienceDto> getExperiencesByHost(String hostId);
 
-    List<ExperienceEntity> getTopRatedExperiences(int limit);
-
-    List<ExperienceEntity> getMostReservedExperiences(int limit);
 }
