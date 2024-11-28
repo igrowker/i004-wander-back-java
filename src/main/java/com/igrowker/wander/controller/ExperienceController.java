880a24194ea9810d8317756d291e7695a8f4a004
Elías Barrera <ebdev81@gmail.com>
2024-11-28 11:39:46 +0100

    feat: enhance search functionality by adding city and country filters
    - Updated `location` handling in `ExperienceEntity` to support city and
    country extraction.
    - Added new repository methods for city and country queries.
    - Refactored `ExperienceServiceImpl` to include advanced filters for
    city and country.
    - Improved logic clarity by replacing complex `else if` chains with
    standalone `if` conditions.

--- src/main/java/com/igrowker/wander/controller/ExperienceController.java
+++ src/main/java/com/igrowker/wander/controller/ExperienceController.java
@@ -44,6 +44,9 @@ public class ExperienceController {
 	        @RequestParam(required = false) Double maxPrice,
 	        @RequestParam(required = false) String title) {
 	    try {
+	        String city = (location != null && location.size() > 1) ? location.get(location.size() - 2) : null;
+	        String country = (location != null && location.size() > 2) ? location.get(location.size() - 1) : null;
+
 	        List<ExperienceEntity> experiences = experienceService.getExperiences(location, maxPrice, title);
 	        return ResponseEntity.ok(experiences);
 	    } catch (IllegalArgumentException e) {
@@ -52,8 +55,6 @@ public class ExperienceController {
 	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 	    }
 	}
-
-
 
 	@GetMapping("/{id}")
 	public ResponseEntity<ExperienceEntity> getExperienceById(@PathVariable String id) {
